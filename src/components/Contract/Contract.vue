6553f381ce1e59aa99b1fcd36abdb39292df518f
Qjin <reactqjin@gmail.com>
2022-08-04 15:30:16 +0900

    folder 구분

---
+++ src/components/Contract/Contract.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>계약서 작성</h2>
+</template>
+
+<script>
+export default {
+  name: 'Contracct',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
