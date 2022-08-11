c9e14f9b5082c116260fbc87ad6a92216741129b
edd3173 <stargazers737@gmail.com>
2022-08-11 11:18:03 +0900

    Add: Chatting Layout

---
+++ src/components/Chat/FooterLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+.footer {
+  width: 100%;
+  height: 44px;
+  text-align: center;
+  background-color: white;
+  vertical-align: bottom;
+}
+
+</style>
