455a7dcd61d5d6af2731d8d38f2c824cc45b715d
bbinibini <gyqls1494@gmail.com>
2022-08-12 09:08:39 +0900

    headerlayout 파일 추가

---
+++ src/HeaderLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header" >
+    <h2 style="float: left;">
+        &nbsp; &lt; &nbsp; {{ title }}
+    </h2>
+    <div class="" style="float: right; padding-top: 15px; padding-right: 10px">
+      <img src="@/assets/hamResized.png" alt="menuIcon">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderLayout",
+    props: {
+        title: String,
+    }
+};
+</script>
+
+<style scoped>
+
+/* 폰트 import후 사용 */
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400&display=swap');
+
+/* 헤더 디자인 */
+.header {
+  text-align: center;
+  background-color: white;
+  width: 100%;
+  height: 44px;
+}
+
+/* 헤더 텍스트 색상 */
+
+h2, h3, h4, h5, h6 {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 100;
+  font-size: 16px;
+  line-height: 30px;
+  color: #000000;
+  padding-top: 15px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
