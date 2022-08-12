c6070405f25976572dbdab88414fd23212373f99
이동희 <eastshine96@naver.com>
2022-08-12 16:56:01 +0900

    ddd

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from "axios"
 // import store from './store'
 let HOST=process.env.HOST
 
-axios.defaults.baseURL = HOST
+axios.defaults.baseURL = 'nexthouse.169.56.100.104.nip.io'
 const app = createApp(App)
 app.config.globalProperties.$axios = axios; 
 app.use(router).mount('#app')
