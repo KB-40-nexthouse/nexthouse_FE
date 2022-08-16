f6f8aff1f677b1bfb2e651ba4aadd9184fbdb8cf
bbinibini <gyqls1494@gmail.com>
2022-08-16 14:21:07 +0900

    bootstrap vue 3 추가

--- src/NewMain.vue
+++ src/NewMain.vue
@@ -1,7 +1,10 @@
 <template>
 <Header title="계약 진행" />
 <body>
-    ㅇㅇㅇ
+<b-button>Button</b-button>
+<b-button variant="danger">Button</b-button>
+<b-button variant="success">Button</b-button>
+<b-button variant="outline-primary">Button</b-button>
 </body>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import { createApp } from 'vue'
-//import App from './MyContract.vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios"
-//import AxiosPlugin from 'vue-axios-cors'
-// import store from './store'
-//axios.defaults.baseURL = process.env.HOST + '/'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios; 
-//axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-//.use(AxiosPlugin)
-//axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
-//axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-app.use(router).mount('#app')
+app.use(BootstrapVue3).use(router).mount('#app')
 //app.mount('#app')
