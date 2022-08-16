9e92d9e6aabb3a7170fa2be943d8612d646be4fa
bbinibini <gyqls1494@gmail.com>
2022-08-16 11:19:13 +0900

    New Main 화면 추가

---
+++ src/NewMain.vue
@@ -0,0 +1,23 @@
+<template>
+<Header title="계약 진행" />
+<body>
+    ㅇㅇㅇ
+</body>
+</template>
+
+<script>
+import Header from "./HeaderLayout.vue";
+export default {
+  name: 'NewStaging',
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style scoped>
+  #body {
+    font-size: 14px;
+    background-color: rgb(251,251,251);
+  }
+</style>
